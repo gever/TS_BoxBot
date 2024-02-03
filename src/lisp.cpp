e0c224890552ec9489258fc243db89c5cfd7af37
Gever Tulley <gever@sfbrightworks.org>
2024-02-03 13:28:04 -0800

    Remove lisp components

--- src/lisp.cpp
+++
@@ -1,9 +0,0 @@
-#ifdef LISP_ENABLED
-#include <iostream>
-#include <string>
-#include "lisp.h"
-
-// setting up lisp runtime
-typedef Lisp<8192, 2048> MySmallLisp;
-MySmallLisp lisp;
-#endif // LISP_ENABLED
