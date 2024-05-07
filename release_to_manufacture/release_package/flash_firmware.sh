c20cf250ed2bded2315d0a02a8c7dbfafaec7ef2
Gever Tulley <gever@sfbrightworks.org>
2024-05-07 15:47:30 -0700

    update the way we access esptool.py

--- release_to_manufacture/release_package/flash_firmware.sh
+++ release_to_manufacture/release_package/flash_firmware.sh
@@ -16,21 +16,22 @@ fi
 echo "Using $PYTHON_CMD for flashing..."
 
 # Check if esptool is installed
-if ! $PYTHON_CMD -m esptool --version &> /dev/null
+$ESP_CMD="esptool.py"
+if ! $ESP_CMD --version &> /dev/null
 then
-    echo "esptool is not installed. Installing esptool..."
-    $PYTHON_CMD -m pip install -r requirements.txt
+    echo "esptool.py is not installed"
+    exit
 fi
 
 echo "Erasing Flash..."
-$PYTHON_CMD -m esptool --chip esp32 --port "$1" --baud 115200 erase_flash
+$ESP_CMD --chip esp32 --port "$1" --baud 115200 erase_flash
 
 echo "Flashing Firmware..."
-$PYTHON_CMD -m esptool --chip esp32 --port "$1" --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 bootloader.bin 0x10000 firmware.bin 0x8000 partitions.bin
+$ESP_CMD --chip esp32 --port "$1" --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 bootloader.bin 0x10000 firmware.bin 0x8000 partitions.bin
 
 echo "Flashing File System..."
 # use ESP-IDF parttool.py  - determine the address is 0x290000 Add command to upload SPIFFS image here
-$PYTHON_CMD -m esptool --chip esp32 --port "$1" --baud 115200 write_flash 0x290000 spiffs.bin
+$ESP_CMD --chip esp32 --port "$1" --baud 115200 write_flash 0x290000 spiffs.bin
 
 
 echo "Firmware has been flashed successfully!"
