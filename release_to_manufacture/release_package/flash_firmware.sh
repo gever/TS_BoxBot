f94c341d2bf442066874dcf4ea8904d10596d6cc
naomihyun <148907868+naomihyun@users.noreply.github.com>
2024-05-08 15:52:17 -0700

    Fix flash_firmware.sh checking of esptool.py install

--- release_to_manufacture/release_package/flash_firmware.sh
+++ release_to_manufacture/release_package/flash_firmware.sh
@@ -17,7 +17,7 @@ echo "Using $PYTHON_CMD for flashing..."
 
 # Check if esptool is installed
 ESP_CMD="esptool.py"
-if ! $ESP_CMD --version &> /dev/null
+if ! $ESP_CMD version &> /dev/null
 then
     echo "esptool.py is not installed"
     exit
